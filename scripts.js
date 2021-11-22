cf4bd9b81e3dd8e8c145fed72ad1e57a3708981d
Yuri Laurindo <94491525+YuriKDeveloper@users.noreply.github.com>
2021-11-22 15:34:42 -0300

    Projeto calculadora Html,css.js

---
+++ scripts.js
@@ -0,0 +1,39 @@
+ const numberButtons = document.querySelectorAll('[data-number]')
+ const operationButtons = document.querySelectorAll('[data-operation]')
+ const equalsButton = document.querySelector('[data-equals]')
+ const deleteButton = document.querySelector('[data-delete]')
+ const allClearButton = document.querySelector('[data-all-clear]')
+ const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+ const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+ 
+
+ const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            calculator.appendNumber(button.innerText)
+            calculator.updateDisplay()
+        })
+
+    })
+
+    operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+        })
+    })
+    equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+    deleteButton.addEventListener('click', button => {
+    calculator.delete ()
+    calculator.updateDisplay()
+    })
+
+    allClearButton.addEventListener('click', button => {
+    calculator.clear()
+})
